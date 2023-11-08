b7df82126575485ac3daa8f5114705347385729a
ricardo1021 <ricardo10_21@hotmail.com>
2023-11-08 06:58:58 -0500

    Add configurations

--- weather/src/main/java/com/pfcti/weather/controller/WeatherController.java
+++ weather/src/main/java/com/pfcti/weather/controller/WeatherController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.pfcti.weather.dto.ServiceResponse;
 import com.pfcti.weather.dto.WeatherRequest;
 import com.pfcti.weather.service.IWeatherService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping(value = "/api/v1/weather")
@@ -19,6 +22,7 @@ public class WeatherController {
     @Autowired
     IWeatherService weatherService;
 
+    @CircuitBreaker(name = "weather", fallbackMethod = "fallbackMethod")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ServiceResponse> getWeather(@RequestBody WeatherRequest weatherRequest) throws JsonProcessingException {
             return new ResponseEntity<>(weatherService.getWeatherData(weatherRequest), HttpStatus.OK);
@@ -29,6 +33,11 @@ public class WeatherController {
     @ResponseStatus(HttpStatus.OK)
     public List<ServiceResponse> getWeatherHistoryData() {
         return weatherService.getWeatherHistoryData();
+    }
+
+    public String fallbackMethod(ResponseEntity weatherRequest, RuntimeException runtimeException) {
+//        log.info("Executing Fallback method");
+        return "Error consuming weather client api!";
     }
 
 }
